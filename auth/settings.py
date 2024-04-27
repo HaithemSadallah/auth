d14213ecb2894049d1d8c6b6d3efb578f9f1070f
HaithemSadallah <haithemsadallah@gmail.com>
2024-04-27 09:24:34 +0100

    tt

--- auth/settings.py
+++ auth/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.environ.get("SECRET_KEY")
+SECRET_KEY =  os.environ.get("SECRET_KEY")  
 # SECRET_KEY = 'django-insecure-#=-fo=9w=o-&uw)4imojgur-sp2!azpexi35ya&j$$sts+gn6+'
 
 
