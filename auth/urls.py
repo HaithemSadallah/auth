a73a09e43f0dc06acd20e170f4d705a9aafaa145
HaithemSadallah <haithemsadallah@gmail.com>
2024-04-27 08:56:13 +0100

    new

--- auth/urls.py
+++ auth/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('account.urls')),
     path('api/', include('product.urls')),
 ]
+urlpatterns+=staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'utils.error_view.handler404'
